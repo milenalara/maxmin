00856bfa912a4261e80a7281d88ddd578611d8ed
Milena Lara <milenalrf@gmail.com>
2025-03-24 21:19:05 -0300

    maxAndMin function created on main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+
+def maxAndMin(numbers):
+    if len(numbers) == 1:
+        return [numbers[0], numbers[0]] # base case
+
+    middle = math.floor(len(numbers) / 2)
+
+    max_min_left = maxAndMin(numbers[:middle])
+    max_min_right = maxAndMin(numbers[middle:])
+
+    # returns the max value and the min value as an array: [max, min]
+    return [max(max_min_left[0], max_min_right[0]), min(max_min_left[1], max_min_right[1])]
